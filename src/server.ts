f84a2287414b7573b8dfb297a8f004ed9ab1b848
jubayerCodes <jubayerhossain111220@gmail.com>
2025-07-03 22:33:32 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from "./app";
+const mongoose = require('mongoose');
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = 3000
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.opkciwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
